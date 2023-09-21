032527279a68fddae156a47dcef73462f392e7f4
Maxim_Gavrikov <destrallhumn@gmail.com>
2023-09-21 20:02:42 +0300

    first commit

--- src/test/java/guru/qa/SimpleTests.java
+++ src/test/java/guru/qa/SimpleTests.java
@@ -1,10 +1,12 @@
 package guru.qa;
 
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@Tag("simple")
 public class SimpleTests {
 
     @Test
